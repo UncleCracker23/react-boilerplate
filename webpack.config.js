7234fd6925bb2232dcbf375c412dd6426563ea30
UncleCracker23 <christophkreutzer23@gmail.com>
2019-12-12 13:24:22 +0100

    pack css into file instead of head tag

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
 	entry: './src/index.js',
@@ -18,13 +19,31 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							// you can specify a publicPath here
+							// by default it uses publicPath in webpackOptions.output
+							publicPath: './src',
+							hmr: process.env.NODE_ENV === 'development',
+						},
+					},
+					'css-loader',
+				],
 			},
 		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
+		}),
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// all options are optional
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+			ignoreOrder: false, // Enable to remove warnings about conflicting order
 		}),
 	],
 }
