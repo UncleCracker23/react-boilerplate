44d7bcb5630fe8a5325b276f17109883bbc4e9d9
Christoph Kreutzer <christoph.kreutzer@kimeta.de>
2019-12-12 11:43:49 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const electron = require('electron')
+
+const { app, BrowserWindow } = electron
+
+let mainWindow
+
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
+
+	mainWindow.loadFile('./dist/index.html')
+})
